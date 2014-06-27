9fc117cbb20305ff3fee2c2fe2a5e4b4e40fda77
Matt Cano <cano.matt@gmail.com>
2014-06-26 17:33:05 -0700

    Made title more dynamic. Will not add separation bar if no title given.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,13 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end 
+
+  def full_title(page_title)
+  	base_title = "PillBox"
+  	if page_title.empty?
+  		base_title
+  	else
+  		"#{base_title} | #{page_title}"
+  	end
+  end
 end
