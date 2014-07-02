7b15d5412047436bf43cde71c5dc71a1ac6b85df
Matt Cano <cano.matt@gmail.com>
2014-07-02 00:56:05 -0700

    Should be complete. Going to run rake migrate and relaunch. Fingers crossed.

--- app/models/coaches_dependent.rb
+++ app/models/coaches_dependent.rb
@@ -1,5 +1,5 @@
 class CoachesDependent < ActiveRecord::Base
-  attr_accessible :coach_id, :dependent_id
+  # attr_accessible :coach_id, :dependent_id
 
   belongs_to :coach, :class_name => "User"
   belongs_to :dependent, :class_name => "User"
