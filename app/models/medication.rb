5d81912656a61abe370179be98f8166a233c8644
Matt Cano <cano.matt@gmail.com>
2014-07-02 00:51:16 -0700

    Remove attr_accessible from medication model.

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,6 +1,4 @@
 class Medication < ActiveRecord::Base
-  attr_accessible :bottle_size, :dosage_quant, :dosage_size, :drug, :frequency,
-                  :name, :notes, :period, :user_id, :added_by
 
   belongs_to :user
   has_many :reminders
