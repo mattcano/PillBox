fc6e280ab855980be26d59804214572fd0caf5d9
Damola Omotosho <damola.omotosho@gmail.com>
2015-02-18 00:26:13 -0800

    Add medicine delete and edit capability

--- src/DotNet/DogFood/PillBox/PillBox.Website/Controllers/AdminController.cs
+++ src/DotNet/DogFood/PillBox/PillBox.Website/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using PillBox.Services;
 using PillBox.Website.Models;
 using PillBox.Website.ScheduledTasks;
 using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -54,12 +55,12 @@ namespace PillBox.Website.Controllers
 
         public async Task<ActionResult> Edit(string id)
         {
-            if (id == null)         
+            if (id == null)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             PillBoxUser user = await UserManager.FindByIdAsync(id);
 
-            if (user == null)  
+            if (user == null)
                 return RedirectToAction("Dashboard");
 
             return View(user);
@@ -78,12 +79,12 @@ namespace PillBox.Website.Controllers
                 PillBoxUser user = await UserManager.FindByIdAsync(formUser.Id);
                 if (user != null)
                 {
-                    if (PropertyChanged(user.FirstName, formUser.FirstName))    user.FirstName = formUser.FirstName;
-                    if (PropertyChanged(user.LastName, formUser.LastName))      user.LastName = formUser.LastName;
-                    if (PropertyChanged(user.Email, formUser.Email))            user.Email = formUser.Email;
-                    if (PropertyChanged(user.PhoneNumber, formUser.PhoneNumber))user.PhoneNumber = formUser.PhoneNumber;
-                    if (PropertyChanged(user.Gender, formUser.Gender))          user.Gender = formUser.Gender;
-                    if (PropertyChanged(user.AgeGroup, formUser.AgeGroup))      user.AgeGroup = formUser.AgeGroup;
+                    if (PropertyChanged(user.FirstName, formUser.FirstName)) user.FirstName = formUser.FirstName;
+                    if (PropertyChanged(user.LastName, formUser.LastName)) user.LastName = formUser.LastName;
+                    if (PropertyChanged(user.Email, formUser.Email)) user.Email = formUser.Email;
+                    if (PropertyChanged(user.PhoneNumber, formUser.PhoneNumber)) user.PhoneNumber = formUser.PhoneNumber;
+                    if (PropertyChanged(user.Gender, formUser.Gender)) user.Gender = formUser.Gender;
+                    if (PropertyChanged(user.AgeGroup, formUser.AgeGroup)) user.AgeGroup = formUser.AgeGroup;
 
                     IdentityResult validUser = await UserManager.UserValidator.ValidateAsync(user);
 
@@ -111,31 +112,94 @@ namespace PillBox.Website.Controllers
             return View("Error", errorList);
         }
 
-        public ActionResult DeleteMed(int medId)
+        public async Task<ActionResult> DeleteMed(int id)
         {
-            //TODO Write Delete Code
-            return View();
+
+            Medicine med = db.Medicines.Include("User").Single(m => m.Id == id);
+            log.Info("Begin deleting " + med.Name + " for " + med.User.FirstName + " MedId: " + id);
+
+            if (med != null)
+            {
+                //TODO Figure out how to do this with Cascade Deletes
+                var userReminders = db.Reminders.Where(m => m.MedicineId == id);
+                log.Info("Deleting " + userReminders.Count() + " reminders.");
+                foreach (var reminder in userReminders)
+                {
+                    log.Info("Delete ReminderId: " + reminder.Id);
+                    db.Reminders.Remove(reminder);
+                }
+
+                await db.SaveChangesAsync();
+
+                //TODO Figure out how to do this with Cascade Deletes & Identity
+                // First Delete User Meds
+                log.Info("Deleting " + med.Name + " from DB");
+
+                log.Info("Delete MedicineId: " + med.Id);
+                JobScheduler.RemoveJob(med.Id);
+                db.Medicines.Remove(med);
+
+                await db.SaveChangesAsync();
+
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            else
+            {
+                log.Info("Med with id: " + id + " not found to delete");
+                return View("Error", new string[] { "Medicine Not Found" });
+            }
         }
 
-        public async Task<ActionResult> EditMed(int medId)
+        public async Task<ActionResult> EditMed(int id)
         {
-            if (medId == null)
+            if (id == 0)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-            Medicine med = await db.Set<Medicine>().FindAsync(medId);
+            Medicine med = await db.Medicines.Include("User").SingleAsync(m => m.Id == id);
 
             if (med == null)
                 return View("Error", new string[] { "User Not Found" });
-                
+
             //TODO finish edit view
             return View(med);
         }
 
         [HttpPost]
-        public ActionResult EditMed(Medicine med)
+        public async Task<ActionResult> EditMed(Medicine formMed)
         {
-            //TODO Write Edit Post Code
-            return View();
+            if (ModelState.IsValid)
+            {
+                Medicine med = db.Medicines.Include("User").Single(m => m.Id == formMed.Id);
+
+                if (med != null)
+                {
+                    if (PropertyChanged(med.Name, formMed.Name)) med.Name = formMed.Name;
+                    if (PropertyChanged(med.RemindTime.ToString(), formMed.RemindTime.ToString())) med.RemindTime = formMed.RemindTime;
+
+                    db.Medicines.Attach(med);
+                    db.Entry(med).State = EntityState.Modified;
+
+                    try
+                    {
+                        log.Info("Editing MedicineId: " + med.Id+" with values "+med.Name+" "+ med.RemindTime.Value.ToShortTimeString());
+                        await db.SaveChangesAsync();
+                        JobScheduler.RemoveJob(med.Id);
+                        JobScheduler.ScheduleMedicineReminder(med);
+                        return RedirectToAction("Edit", "Admin", new { id = med.User.Id });
+                    }
+                    catch
+                    {
+                        log.Info("Error while editing Med with id: " + formMed.Id);
+                        return View("Error", new string[] { "Medicine Edit Error" });
+                    }
+                }
+            }
+
+            var errorList = ModelState.Values.SelectMany(m => m.Errors)
+                            .Select(e => e.ErrorMessage)
+                            .ToList();
+
+            return View("Error", errorList);
         }
 
         [HttpPost]
